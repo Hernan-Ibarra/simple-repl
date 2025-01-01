d2af017855e4bf6a0b607125bc41e864352dce23
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-01-01 15:15:10 +0000

    .

--- lua/interaction.lua
+++ lua/interaction.lua
@@ -18,7 +18,7 @@ end
 M.send_file = function()
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   send_to_repl(table.insert(lines, ''))
-  print(lines)
+  print(table.concat(lines, ', '))
 end
 
 -- Send the selected text to the REPL

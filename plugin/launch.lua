1d90f1ae443b81f7e5ffff238037aa6dec004155
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-01-02 22:01:06 +0000

    Added LuaCATS annotations to the whole project now, and abstracted making commands + setting keymaps at launch

--- plugin/launch.lua
+++ plugin/launch.lua
@@ -1,29 +1,42 @@
-local usr_command = vim.api.nvim_create_user_command
+---@alias library "interaction" | "display"
 
-usr_command('ReplToggle', function()
-  require('display').toggle_repl()
-end, {})
-usr_command('ReplOpen', function()
-  require('display').open_repl()
-end, {})
-usr_command('ReplClose', function()
-  require('display').close_repl()
-end, {})
-usr_command('ReplHide', function()
-  require('display').hide_repl()
-end, {})
+-- Makes a command 'command_name' which executes 'method_name' from 'library'
+---@param command_name string
+---@param library library
+---@param method_name string
+---@return nil
+local make_command = function(command_name, library, method_name)
+  local callback = function()
+    require(library)[method_name]()
+  end
+  vim.api.nvim_create_user_command(command_name, callback, {})
+end
 
-usr_command('ReplSendLine', function()
-  require('interaction').send_line()
-end, {})
-usr_command('ReplSendLineExecute', function()
-  require('interaction').send_line_and_execute()
-end, {})
+local prefix = '<leader>r'
 
-vim.keymap.set('n', '<leader>rt', '<cmd>ReplToggle<CR>')
-vim.keymap.set('n', '<leader>ro', '<cmd>ReplOpen<CR>')
-vim.keymap.set('n', '<leader>rc', '<cmd>ReplClose<CR>')
-vim.keymap.set('n', '<leader>rh', '<cmd>ReplHide<CR>')
+-- Shortcut "prefix .. key" in normal mode for "command_name"
+---@param command_name string
+---@param key string
+---@return nil
+local set_key_normal = function(command_name, key)
+  vim.keymap.set('n', prefix .. key, '<cmd>' .. command_name .. '<CR>')
+end
 
-vim.keymap.set('n', '<leader>rl', '<cmd>ReplSendLine<CR>')
-vim.keymap.set('n', '<leader>rr', '<cmd>ReplSendLineExecute<CR>')
+-- A wrapper for make_command and set_key_normal in succession
+---@param command_name string
+---@param library library
+---@param method_name string
+---@param key string
+---@return nil
+local make_command_and_keymap = function(command_name, library, method_name, key)
+  make_command(command_name, library, method_name)
+  set_key_normal(command_name, key)
+end
+
+make_command_and_keymap('ReplToggle', 'display', 'toggle_repl', 't')
+make_command_and_keymap('ReplOpen', 'display', 'open_repl', 'o')
+make_command_and_keymap('ReplClose', 'display', 'close_repl', 'c')
+make_command_and_keymap('ReplHide', 'display', 'hide_repl', 'h')
+
+make_command_and_keymap('ReplSendLine', 'interaction', 'send_line', 'l')
+make_command_and_keymap('ReplSendLineExecute', 'interaction', 'send_line_and_execute', 'r')
